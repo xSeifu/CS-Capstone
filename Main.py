a6819bd700d6cdcc3d452de3b15dca421aead4a5
Taylor Coulter <tcoul19@wgu.edu>
2025-01-02 18:50:13 -0500

    Changed csv name

--- Main.py
+++ Main.py
@@ -1,4 +1,10 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('CarsDataset.csv')
 
 st.title('CS CAPSTONE PROJECT')
+
+st.subheader('Data Information:')
+st.write(df.describe())
