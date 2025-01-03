aa4b33c96599d799cf3049cd19a0236750943f5e
Taylor Coulter <tcoul19@wgu.edu>
2025-01-03 18:08:34 -0500

    Converted values in CSV to numbers (Prices, horsepower, 0-100km/h) Added 2 visuals, Pie chart showing car company percentages represented and scatterplot of cost of vehicle in relation to the amount of horsepower

--- Main.py
+++ Main.py
@@ -9,3 +9,27 @@ st.title('CS CAPSTONE PROJECT')
 
 st.subheader('Data Information:')
 st.write(df.describe())
+
+# Pie chart of car manufacturers
+manufacturer_counts = df['Company Names'].value_counts()
+threshold = 0.02 * manufacturer_counts.sum()
+manufacturer_counts['Other'] = manufacturer_counts[manufacturer_counts < threshold].sum()
+manufacturer_counts = manufacturer_counts[manufacturer_counts >= threshold]
+fig, ax = plt.subplots()
+ax.pie(manufacturer_counts, labels=manufacturer_counts.index, autopct='%1.1f%%', startangle=90, colors=plt.cm.Paired.colors)
+ax.set_title('Car Distribution by Manufacturer')
+st.pyplot(fig)
+
+# Scatter plot for Price vs Horsepower
+price_threshold = 1000000
+filtered_df = df[df['Car Prices$'] <= price_threshold]
+fig, ax = plt.subplots()
+ax.scatter(filtered_df['HorsePower'], filtered_df['Car Prices$'], alpha=0.5)
+ax.set_title('Price vs. Horsepower')
+ax.set_xlabel('Horsepower')
+ax.set_ylabel('Price (in $)')
+price_ticks = np.arange(0, filtered_df['Car Prices$'].max(), 50000)
+horsepower_ticks = np.arange(0, filtered_df['HorsePower'].max() + 50, 150)
+ax.set_xticks(horsepower_ticks)
+ax.set_yticks(price_ticks)
+st.pyplot(fig)
