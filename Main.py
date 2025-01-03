e745f92cd5b26fa5417da25f92c1bea3f2df7541
Taylor Coulter <tcoul19@wgu.edu>
2025-01-02 20:12:34 -0500

    Cleaned up CSV, got python imports working

--- Main.py
+++ Main.py
@@ -1,8 +1,9 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('CarsDataset.csv')
+df = pd.read_csv('CarsDataset.csv', encoding='cp1252')
 
 st.title('CS CAPSTONE PROJECT')
 
